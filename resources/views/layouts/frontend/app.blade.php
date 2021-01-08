22871e0b735e68eb7c12d0012af2174951f064b6
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-01-08 14:43:43 +0600

    laravel 7 blog project

---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Clean Blog - Start Bootstrap Theme</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="{{ asset('frontend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('frontend/css/clean-blog.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+@include('layouts.frontend.header');
+
+  <!-- Main Content -->
+ 
+
+  <!-- Footer -->
+  @include('layouts.frontend.footer');
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="{{ asset('frontend/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{ asset('frontend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="{{ asset('frontend/js/clean-blog.min.js')}}"></script>
+
+</body>
+
+</html>
